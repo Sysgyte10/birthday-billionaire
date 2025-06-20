ad5dd379a76cce7dc69a706c1ae8bbeda22a8782
AdebayoBunmiSolomon <hardeybayorsamuel11@gmail.com>
2025-06-20 06:30:40 +0100

    First commit

---
+++ src/resources/color/color.tsx
@@ -0,0 +1,7 @@
+export enum colors {
+  goldenRod = "#CA9222",
+  black = "#484848",
+  lightGrey = "#F5F5F5",
+  darkGrey = "#717171",
+  blue = "#407BFF",
+}
---
+++ src/router/types.ts
@@ -0,0 +1,42 @@
+import { type ParamListBase } from "@react-navigation/native";
+import { type NativeStackScreenProps } from "@react-navigation/native-stack";
+import { type BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+
+//auth screen stack navigation
+export interface AuthStackParamList extends ParamListBase {
+  Onboarding: undefined;
+  GetStarted: undefined;
+  SignUp: undefined;
+  Login: undefined;
+  ForgotPassword: undefined;
+  VerificationCode: undefined;
+  ChangePassword: undefined;
+}
+
+export type AuthScreenProps<ScreenName extends keyof AuthStackParamList> =
+  NativeStackScreenProps<AuthStackParamList, ScreenName>;
+
+//bottom tab-bar screen navigation
+export interface BottomTabBarStackParamList extends ParamListBase {
+  HomeStack: undefined;
+  LotteryStack: undefined;
+  MessageStack: undefined;
+  GiftStack: undefined;
+  ProfileStack: undefined;
+}
+
+export type BottomTabBarScreenProps<
+  ScreenName extends keyof BottomTabBarStackParamList
+> = BottomTabScreenProps<BottomTabBarStackParamList, ScreenName>;
+
+//native and app screen navigation
+export interface RootStackParamList extends ParamListBase {
+  Home: undefined;
+  Lottery: undefined;
+  Message: undefined;
+  Gift: undefined;
+  Profile: undefined;
+}
+
+export type RootStackScreenProps<ScreenName extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, ScreenName>;
